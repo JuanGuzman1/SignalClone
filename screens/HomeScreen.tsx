f15019889c55ec1380e7b2ad6e0ba7b3c3d90f52
Juan Guzman <juanltd07@gmail.com>
2021-11-01 12:58:07 -0600

    amplify and authentication

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,10 +1,15 @@
 import * as React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Pressable } from "react-native";
 import ChatRoomItem from "../components/ChatRoomItem";
 import chatRoomsData from "../assets/dummy-data/ChatRooms";
 import { FlatList } from "react-native-gesture-handler";
+import { Auth } from "aws-amplify";
 
 export default function TabOneScreen() {
+  const logOut = () => {
+    Auth.signOut();
+  };
+
   return (
     <View style={styles.page}>
       <FlatList
@@ -12,6 +17,19 @@ export default function TabOneScreen() {
         renderItem={({ item }) => <ChatRoomItem chatRoom={item} />}
         showsVerticalScrollIndicator={false}
       />
+      {/* <Pressable
+        onPress={logOut}
+        style={{
+          backgroundColor: "red",
+          height: 50,
+          margin: 10,
+          alignItems: "center",
+          justifyContent: 'center',
+          borderRadius: 50,
+        }}
+      >
+        <Text>Logout</Text>
+      </Pressable> */}
     </View>
   );
 }
